1afc7dfc1db5d1b892d9ec94acba8205a1d2cb8c
balabala-ccgk <54793210+balabala-ccgk@users.noreply.github.com>
2019-09-20 20:13:35 +0800

    Create Practise1.java

---
+++ Practise1.java
@@ -0,0 +1,16 @@
+/**
+* Description: Calculation
+* Author:      lalala
+* Date:        2019-9-16
+*/
+public class Practise1 {
+    /**
+    * Description: Calculation
+    * param:       [null]
+    * return:      [null]
+	* Date:        2019-9-16
+    */  
+  public static void main(String[] args){
+		System.out.println("34 + 30 = " +34+30);
+	}
+}
