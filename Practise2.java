7a71a971ae5b7611ada91dfb16f84c16f036a1bd
balabala-ccgk <54793210+balabala-ccgk@users.noreply.github.com>
2019-09-20 20:17:33 +0800

    Create Practise2.java

---
+++ Practise2.java
@@ -0,0 +1,24 @@
+/**
+* Description: Calculation count
+* Author:      lalala
+* Date:        2019-9-16
+*/
+public class Practise2{
+	/**
+  * Description: Calculation count
+  * param:       [null]
+  * return:      [null]
+  * Date:        2019-9-16
+  */
+	public static void main(String[] args){
+		int count = 10;
+		count *= count + 2;
+		count /= 100;
+		count ++;
+		System.out.println("count = " + count);
+		
+	}
+	
+	
+	
+}
