c7963888e8ee3b699edb924b3ed3fb84452945a6
balabala-ccgk <54793210+balabala-ccgk@users.noreply.github.com>
2019-09-20 20:19:54 +0800

    Create Practise3.java

---
+++ Practise3.java
@@ -0,0 +1,116 @@
+/**
+* Description: Calculation
+* Author:      lalala
+* Date:        2019-9-16
+*/
+
+public class Practise3{
+    /**
+    * Description: Calculation
+    * param:       [null]
+    * return:      [null]
+	  * Date:        2019-9-16
+    */
+	
+	public static int one(){
+		int result = 0;
+		int n1 = 12;
+		int n4 = 3;
+		result = n1 / n4;
+		System.out.println("res = n1 / n4 = " + result);
+		return 0;
+	}
+	public static int two(){
+		double d;
+		int n1 = 12;
+		int n4 = 3;
+		d = n1 / n4;
+		System.out.println("d = n1 / n4 = " + d);
+		return 0;
+	}	
+	public static int three(){
+		int result;
+		int n3 = 16;
+		int n4 = 3;
+		result = n3 / n4;
+		System.out.println("result = n3 / n4 = " + result);
+		return 0;
+	}	
+	public static int four(){
+		double d;
+		int n3 = 16;
+		int n4 = 3;
+		d = n3 / n4;
+		System.out.println("d = n3 / n4 = " + d);
+		return 0;
+	}
+    public static int five(){
+		double d;
+		double v1 = 10.0;
+		int n4 = 3;
+		d = v1 / n4;
+		System.out.println("d = v1 / n4 = " + d);
+		return 0;
+	}		
+	public static int six(){
+		double d;
+		double v1 = 10.0;
+		double v2 = 3.1416;
+		d = v1 / v2;
+		System.out.println("d = v1 / v2" + d);
+		return 0;
+	}		
+	public static int seven(){
+		double result;
+		double n1 = 12;
+		double n2 = 24;
+		result = n1 / n2;
+		System.out.println("result = n1 / n2 = " + result);
+		return 0;
+	}
+	public static int eight(){
+        double d;
+        double n1 = 12;
+        int n2 = 24;	
+	    d = n1 / n2;
+		System.out.println("d = n1/n2 = " + d);
+		return 0;
+	}
+	public static int nine(){
+        double d;
+        int n1 = 12;
+        double n2 = 24;	
+	    d = n1 / n2;
+		System.out.println("d = n1/n2 = " + d);
+		return 0;
+	}
+	public static int ten(){
+        double d;
+        double n1 = 12;
+        double n2 = 24;	
+	    d = n1 / n2;
+		System.out.println("d = (double)(n1/n2) = " + d);
+		return 0;
+	}
+ 
+	
+	
+	public static void main (String[] args){	    
+        one();
+		two();
+		three();
+		four();
+		five();
+		six();
+		seven();
+		eight();
+		nine();
+		ten();
+        
+		}			
+	}
+	
+	
+	
+	
+	
