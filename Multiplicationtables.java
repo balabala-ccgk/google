32889736ab8232787ff1ebd448430dc13e9743b0
balabala-ccgk <54793210+balabala-ccgk@users.noreply.github.com>
2019-09-22 19:30:54 +0800

    Update Multiplicationtables.java

--- Multiplicationtables.java
+++ Multiplicationtables.java
@@ -8,7 +8,7 @@ public class Multiplicationtables{
     * Description: Multiplication tables
     * param:       [null]
     * return:      [null]
-	* Date:        2019-9-22
+    * Date:        2019-9-22
     */  
 	public static void main(String[] args){
 		int i,j,k,z;
@@ -18,13 +18,13 @@ public class Multiplicationtables{
 		while(i >= 1){
 			while(j <= 9){
 			    k = i * j;
-                System.out.print(i);	
-				System.out.print(" * ");	
-				System.out.print(j);
-                System.out.print(" = ");
-                System.out.print(k );
-                System.out.print("  ");	
-                j++;				
+                            System.out.print(i);	
+		            System.out.print(" * ");	
+		            System.out.print(j);
+                            System.out.print(" = ");
+                            System.out.print(k );
+                            System.out.print("  ");	
+                            j++;				
 			}
 		i--;	
 		z++;
